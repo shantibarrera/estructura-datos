8cb881836165818929d7273f4fe26b1843603682
Santiago Barrera <98606569+shantibarrera@users.noreply.github.com>
2023-09-16 02:46:58 +0000

    Tareas

--- clase4/ejerciciofinal.py
+++ clase4/ejerciciofinal.py
@@ -1,20 +1,14 @@
 #Importa la libreria random
 import random
 
-#Crea una función para la lista de nodos
-def lista(nodos):
+def lista(nodos): #Crea una función para la lista de nodos
     lista_final = []
-    #Crea un condicional dependiendo de los nodos que quiera crear el usuario
-    for i in range(nodos):
-        #Inserta un número aleatorio con la función random.randint entre i e i + 10
-        numero_aleatorio = random.randint(i, i + 50)
+    for i in range(nodos): #Crea un condicional dependiendo de los nodos que quiera crear el usuario
+        numero_aleatorio = random.randint(i, i + 20) #Inserta un número aleatorio con la función random.randint entre i e i + 10
         lista_final.append(numero_aleatorio)
-    #Nos retorna la lista con los nodos creados
-    return lista_final
+    return lista_final  #Nos retorna la lista con los nodos creados
 
-#Se le pide al usuario el número de nodos que desea crear
-nodos = int(input("Ingrese el número de nodos a crear: "))
+nodos = int(input("Ingrese el número de nodos a crear: ")) #Se le pide al usuario el número de nodos que desea crear
 lista_final = lista(nodos)
 
-#Imprime la lista con los nodos generados
-print("Lista generada:", lista_final)
+print("Lista generada:", lista_final) #Imprime la lista con los nodos generados
--- clase5/ejercicio1.py
+++ clase5/ejercicio1.py
@@ -1,1 +1,59 @@
 """Eliminar Dato de Nodo y eliminar el nodo que quiera"""
+
+class Nodo:
+    def __init__(self, valor):
+        self.valor = valor
+        self.siguiente = None
+
+class Pila:
+    def __init__(self):
+        self.top = None
+
+    def apilar(self, valor):
+        nuevo_nodo = Nodo(valor)
+        nuevo_nodo.siguiente = self.top
+        self.top = nuevo_nodo
+
+    def desapilar(self):
+        if self.top is not None:
+            valor = self.top.valor
+            self.top = self.top.siguiente
+            return valor
+        else:
+            return None
+
+    def recorrido(self):
+        nodo_actual = self.top
+        while nodo_actual is not None:
+            print(nodo_actual.valor)
+            nodo_actual = nodo_actual.siguiente
+
+    def eliminar_nodo_interno(self, valor):
+        nodo_actual = self.top
+        nodo_anterior = None
+
+        while nodo_actual is not None:
+            if nodo_actual.valor == valor:
+                if nodo_anterior is not None:
+                    nodo_anterior.siguiente = nodo_actual.siguiente
+                else:
+                    self.top = nodo_actual.siguiente
+                return
+            nodo_anterior = nodo_actual
+            nodo_actual = nodo_actual.siguiente
+
+if __name__ == "__main__":
+    pila = Pila()
+
+    # Permitir al usuario agregar manualmente hasta 5 datos a la pila
+    for _ in range(5):
+        valor = int(input("Ingrese un valor para apilar en la pila: "))
+        pila.apilar(valor)
+
+    print("Pila original:")
+    pila.recorrido()
+
+    valor_desapilar = int(input("\nIngrese el valor que desea desapilar: "))
+    pila.eliminar_nodo_interno(valor_desapilar)
+    print("\nPila después de desapilar el valor {}:".format(valor_desapilar))
+    pila.recorrido()
